657907e71bb4814b94359b91ddb2ea2d5a86da45
Ali Yasin <46627616+aliyasiny65@users.noreply.github.com>
2022-04-23 00:46:44 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-let sistemtema = window.matchMedia('(prefers-color-scheme: dark)').matches;
-if (sistemtema) {
-    document.getElementsByTagName('body')[0].classList.toggle("dark");
-} else if (!sistemtema) {
-}
