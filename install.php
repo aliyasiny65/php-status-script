b674c5c70f0a3fd656809da4c179cadcfc38d57a
Ali Yasin <46627616+aliyasiny65@users.noreply.github.com>
2022-05-13 23:36:31 +0300

    Update install.php

--- install.php
+++ install.php
@@ -13,8 +13,9 @@ if (isset($_POST['submit'])) {
     $email = $_POST['mail'];
     $password = md5($_POST['passwd']);
     if($conn) {
+	$nowdate = date("h:i");
         $configfile = fopen("config.php", "w") or die("Unable to create config file!");
-        $data = "<?php \n\$sdescription = '$sdescription';\n\$sname = '$sname';\n\$server = '$servername';\n\$dbuser = '$dbuser';\n\$dbpass = '$dbpass';\n\$dbname = '$dbname';\n\$version = '1.5';\n\n\$matomo = 'disabled';\n\$ganalytics = 'disabled';\n\$conn = mysqli_connect(\$server, \$dbuser, \$dbpass, \$dbname);\n\n?>";
+        $data = "<?php \n\$sdescription = '$sdescription';\n\$sname = '$sname';\n\$server = '$servername';\n\$dbuser = '$dbuser';\n\$dbpass = '$dbpass';\n\$dbname = '$dbname';\n\$version = '1.5';\n\n\$matomo = 'disabled';\n\$ganalytics = 'disabled';\n\$conn = mysqli_connect(\$server, \$dbuser, \$dbpass, \$dbname);\n\n\$lastreport = '$nowdate';\n?>";
         fwrite($configfile, $data);
         fclose($configfile);
         $sqlinj = file_get_contents('dbtable.sql');
